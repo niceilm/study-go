f5b6d2643561ef8e4ac5d74c2c5b76701c3cfe85
flynn <niceilm@naver.com>
2017-01-06 13:58:45 +0900

    init files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Study Go
+
+## Syntax
+
+## Build
+
+## Deploy
+
+## Test
+
+## Link
+
+### Official
+- [Go 공식페이지](https://golang.org)
+
+### Study
+- [Go-Tour](http://go-tour-kr.appspot.com/#1)
+
+### Useful
+- [Awesome Go](https://github.com/avelino/awesome-go)
+
+### Slides
+- [Go슬라이드](http://www.slideshare.net/songaal/lets-go-45867246)
---
+++ go_tour/01-hello_test.go
@@ -0,0 +1,20 @@
+package main
+//
+//import (
+//  "testing"
+//  "fmt"
+//)
+//
+//func ExampleHello() {
+//  fmt.Println("hello")
+//}
+//func TestGenerateQRCodeRetrunsValue(t *testing.T) {
+//  result := GenerateQRCode("1234-1234")
+//
+//  if result == nil {
+//    t.Error("Genderated QRCode is nil")
+//  }
+//  if len(result) == 0 {
+//    t.Errorf("Generated QRCode has no data")
+//  }
+//}
